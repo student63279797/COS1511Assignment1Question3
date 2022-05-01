a5a2513b66250dc33ecc8f361daf903e85fcb99e
student63279797 <63279797@mylife.unisa.ac.za>
2022-05-01 17:18:52 +0200

    change

--- main.cpp
+++ main.cpp
@@ -3,19 +3,18 @@
 
 using namespace std;
 
-void askName();
-
 int main()
 {
-    int nrPupils = 56,nrGroups=0,nrLeft=0,groupSize = 0;
-    string str_groupSize = "0";
-    cout << "Please enter the size of each group?" << endl;
-    getline(cin,str_groupSize);
-    groupSize = stoi(str_groupSize);
-    nrGroups = nrPupils / groupSize;
-    nrLeft = nrPupils % groupSize;
-    cout << "There are " << groupSize <<" groups consisting of " << nrGroups <<" pupils There are " << nrLeft <<" remaining pupils" << endl;
-    //getline(cin,str_groupSize);
+    var i = 0;
+    // int nrPupils = 56,nrGroups=0,nrLeft=0,groupSize = 0;
+    // string str_groupSize = "0";
+    // cout << "Please enter the size of each group?" << endl;
+    // getline(cin,str_groupSize);
+    // groupSize = stoi(str_groupSize);
+    // nrGroups = nrPupils / groupSize;
+    // nrLeft = nrPupils % groupSize;
+    // cout << "There are " << groupSize <<" groups consisting of " << nrGroups <<" pupils There are " << nrLeft <<" remaining pupils" << endl;
+    // //getline(cin,str_groupSize);
 
     return 0;
 }
